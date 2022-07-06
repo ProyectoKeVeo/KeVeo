9718594dd073233be88cf12b0ebee98f2f632c19
mrodriguez9711 <105204645+mrodriguez9711@users.noreply.github.com>
2022-07-06 13:03:07 +0200

    Update ClicksEntity.java
    gts y sts

--- src/main/java/com/example/KeVeo/data/entity/ClicksEntity.java
+++ src/main/java/com/example/KeVeo/data/entity/ClicksEntity.java
@@ -20,4 +20,27 @@ public class ClicksEntity {
     // TODO: 05/07/2022 foreign keys
 
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public byte getStep() {
+        return step;
+    }
+
+    public void setStep(byte step) {
+        this.step = step;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
