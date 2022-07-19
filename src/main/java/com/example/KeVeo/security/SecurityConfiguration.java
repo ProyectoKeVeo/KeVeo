1350ebf1753e5b1e5f028207ed86982a81269240
Gemma <gemmaramirezlorido@gmail.com>
2022-07-19 17:36:29 +0200

    Commit de HTML.

--- src/main/java/com/example/KeVeo/security/SecurityConfiguration.java
+++ src/main/java/com/example/KeVeo/security/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -44,8 +43,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
                         "/registro**",
-                        "/js/**",
-                        "/css/**",
+                        "/static/js/**",
+                        "/static/css/**",
                         "/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
