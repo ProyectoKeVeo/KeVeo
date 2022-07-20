05d50b41b066133e537fb7367d78c696ebd811b3
Esteban Torres <estebanto92@gmail.com>
2022-07-20 17:39:00 +0200

    error-login

--- src/main/java/com/example/KeVeo/controller/RegistroControlador.java
+++ src/main/java/com/example/KeVeo/controller/RegistroControlador.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -15,6 +16,12 @@ public class RegistroControlador {
 
     @GetMapping("/login")
     public String iniciarSesion() {
+        return "login";
+    }
+
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("errors", true);
         return "login";
     }
 
